6598cf4ebeaa793f54a500c28f69037938fce97c
Quincy Cheng <quincy.cheng@gmail.com>
2020-09-22 09:33:00 +0800

    Update setupConjur.sh

--- conjur/setupConjur.sh
+++ conjur/setupConjur.sh
@@ -7,7 +7,7 @@ export CONJUR_DATA_KEY="$(< data_key)"
 docker-compose up -d 
 sleep 10
 docker-compose exec conjur conjurctl account create demo | tee admin.out
-docker-compose exec client bash -c "echo yes | conjur init -u $1 -a demo"
+#docker-compose exec client bash -c "echo yes | conjur init -u $1 -a demo"
 
 # sleep 10
 # api_key="$(grep API admin.out | cut -d: -f2 | tr -d ' \r\n')"
