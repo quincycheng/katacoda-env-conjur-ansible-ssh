6b358e7af3be87ba5fc99238189ff48470c303b9
Quincy Cheng <quincy.cheng@gmail.com>
2020-09-22 16:27:49 +0800

    Update setupConjur.sh

--- conjur/setupConjur.sh
+++ conjur/setupConjur.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo service docker restart
 curl -o docker-compose.yml https://quincycheng.github.io/docker-compose.quickstart.yml
 docker-compose pull
 docker-compose run --no-deps --rm conjur data-key generate > data_key
